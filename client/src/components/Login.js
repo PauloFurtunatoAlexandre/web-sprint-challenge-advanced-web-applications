417cf744c6724422395863fcacfda499a4edc4ed
Paulo Alexandre <furtuna.alexandre@gmail.com>
2020-08-14 20:41:27 -0500

    added the token to localStorage

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [login, setLogin] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setLogin({
+      ...login,
+      [e.target.name]: e.target.value,
+    });
+    console.log(login);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/api/login", login)
+      .then((res) => {
+        console.log(res);
+        window.localStorage.setItem("token", res.data.payload);
+      })
+      .catch((err) => console.error("Could not login: ", err.message));
+  };
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          value={login.username}
+          placeholder="Username"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={login.password}
+          placeholder="Password"
+          onChange={handleChange}
+        />
+        <button>Login</button>
+      </form>
     </>
   );
 };
